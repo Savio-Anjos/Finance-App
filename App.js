30db968093c20cbfbea6f8941137f08a5b6495dd
savio <savioanjos2020@gmail.com>
2023-07-12 14:32:46 -0300

    :sparkles: feat: Configurando navegações

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import firebase from "./src/services/firebaseConnection";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#131313" barStyle="light-content" />
+      <Routes />
+    </NavigationContainer>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import AuthRoutes from "./auth.routes";
+//import AppRoutes from "./app.routes";
+
+function Routes() {
+  return <AuthRoutes />;
+}
+
+export default Routes;
