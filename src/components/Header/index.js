a6d2b142b07fd66c67fa5cf6da043c1df8d1eb42
savio <savioanjos2020@gmail.com>
2023-07-20 14:55:57 -0300

    :sparkles: feat: Registrando um gasto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,13 +7,9 @@ import { Container, ButtonMenu } from "./styles";
 export default function Header() {
   const navigation = useNavigation();
 
-  function openDrawer() {
-    navigation.openDrawer();
-  }
-
   return (
     <Container>
-      <ButtonMenu onPress={openDrawer}>
+      <ButtonMenu onPress={navigation.toggleDrawer}>
         <Icon name="menu" color="#FFF" size={30} />
       </ButtonMenu>
     </Container>
